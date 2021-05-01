5d734b2724b4ecd726281a0ad165e9cf72f1bc1b
ally-sorloth <allysorloth@gmail.com>
2021-05-02 02:43:14 +0300

    added context information and indext html

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
 
 def index(request):
-    return render(request, "weather/index.html")
+    url = config("BASE_URL")
+    city = "Berlin"
+    r = requests.get(url.format(city))
+    content = r.json()
+    # print(type(a))
+    # pprint(a)
+    context = {
+        "city": city,
+        "temp": content["main"]["temp"],
+        "description": content["weather"][0]["description"],
+        "icon": content["weather"][0]["icon"]
+    }
+    return render(request, "weather/index.html", context)
 
 # Create your views here.
