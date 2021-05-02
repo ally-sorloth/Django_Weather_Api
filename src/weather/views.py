ab4d8bb33b294328f9afd357058b33f95510b6d3
ally-sorloth <allysorloth@gmail.com>
2021-05-03 00:25:45 +0300

    added city data list in view and index html

--- src/weather/views.py
+++ src/weather/views.py
@@ -2,19 +2,33 @@ from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from .models import City
 
 def index(request):
+    cities = City.objects.all()
     url = config("BASE_URL")
-    city = "Berlin"
-    r = requests.get(url.format(city))
-    content = r.json()
+    
+    
     # print(type(a))
     # pprint(content)
+    city_data = []
+    for city in cities: 
+        # print(city)
+        r = requests.get(url.format(city))
+        content = r.json()
+        
+        weather_data = {
+            "city": city.name,
+            "temp": content["main"]["temp"],
+            "description": content["weather"][0]["description"],
+            "icon": content["weather"][0]["icon"]
+        }
+        
+        city_data.append(weather_data)
+    # print(city_data)
+        
     context = {
-        "city": city,
-        "temp": content["main"]["temp"],
-        "description": content["weather"][0]["description"],
-        "icon": content["weather"][0]["icon"]
+        "city_data": city_data
     }
     return render(request, "weather/index.html", context)
 
